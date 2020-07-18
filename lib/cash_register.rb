a0e4969fd55a8adcf76fe71ace5be07779a81526
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-18 16:51:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,37 +1,37 @@
 
-# class CashRegister
-#   attr_accessor :total, :discount, :last_transaction
+class CashRegister
+  attr_accessor :total, :discount, :last_transaction
   
-#   def initialize(discount = 0)
-#     @discount = discount
-#     @total = 0
-#     @items = []
-#   end
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+  end
   
-#   def add_item(title, price, quantity = 1)
-#     @total +=pricequantity
-#     @last_transaction = pricequantity
-#     quantity.times do #try each here 
-#       @items.push(title)
-#     end 
-#   end 
+  def add_item(title, price, quantity = 1)
+    @total +=pricequantity
+    @last_transaction = pricequantity
+    quantity.times do #try each here 
+      @items.push(title)
+    end 
+  end 
   
-#   def apply_discount
-#     if @discount != 0
-#     @total -= @total*@discount/100
-#     return "After the discount, the total comes to $#{@total}."
-#     else
-#     return "There is no discount to apply."
-#     end
-#     end
+  def apply_discount
+    if @discount != 0
+    @total -= @total*@discount/100
+    return "After the discount, the total comes to $#{@total}."
+    else
+    return "There is no discount to apply."
+    end
+    end
     
-#     def items 
-#       @items 
-#     end 
+    def items 
+      @items 
+    end 
     
-#     def void_last_transaction
-#       last_item = @items.pop()
-#       @total -= @last_transaction
-#     end 
+    def void_last_transaction
+      last_item = @items.pop()
+      @total -= @last_transaction
+    end 
 
-# end 
+end 
